055f2892d5c9b1680d50e94d50fa360ec0ebe4c3
Atshan <ratshan>
2021-05-07 12:06:04 -0500

    Plotting the inventory of NWIS site's temperature volume.

---
+++ code/plot_NWIS_temp_volume.R
@@ -0,0 +1,25 @@
+library("ggplot2")
+theme_set(theme_bw())
+library("sf")
+library("maps")#provides maps of the USA, with state and county borders.
+
+#retrieve USA state map then converting as sf objects.
+states <- st_as_sf(map("state", plot = FALSE, fill = TRUE))
+
+sites_sf <- lake_temp_inventory %>%
+  filter(!(is.na(dec_lat_va) & is.na(dec_long_va))) %>%
+  st_as_sf(coords = c("dec_lat_va", "dec_long_va"), crs = 4326)
+
+p <- ggplot() +
+  geom_sf(data = states, color="black", fill=NA) + # plot state outlines
+  geom_sf(data = sites_sf, aes(geometry = geometry,
+                               size = count_nu)) +
+  scale_fill_viridis_c(trans = "sqrt", alpha = .4) +
+  #scale_fill_viridis(option = "magma", direction = -1)+
+  # setting long and lat limit to grpah contiguous states.  
+  coord_sf(xlim = c(-127, -66), ylim = c(24, 50), expand = FALSE) +
+  
+  xlab("Longitude") + ylab("Latitude") +
+  #classic dark-on-light theme: changing gray background to lighter them
+  theme_bw() 
+

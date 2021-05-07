77470c17c65345c1efa22d33dd1b04bb37553087
Atshan <ratshan>
2021-05-07 15:49:36 -0500

    Keeping track the HUC number to check if any are failing while we're retrieving temperature data inventory.  

--- code/NWIS_reservoir_inventory.R
+++ code/NWIS_reservoir_inventory.R
@@ -12,7 +12,8 @@ inventory_nwis_lakes <- function(site_type, parameter_cd){
                                               siteType = site_type,
                                               parameterCd = parameter_cd) %>%
       select(c("agency_cd", "site_no", "station_nm", "dec_lat_va", 
-               "dec_long_va", "count_nu"))
+               "dec_long_va", "count_nu"))%>%
+      mutate(huc_number = huc)
     
     lake_temp_inventory <- bind_rows(lake_temp_inventory, state_lake_temp_inventory)
     
@@ -23,3 +24,5 @@ inventory_nwis_lakes <- function(site_type, parameter_cd){
 # Testing the inventory_nwis_lakes function
 test_inventory_nwis_lakes <- inventory_nwis_lakes(
   site_type = "LK" ,parameter_cd = "00010")
+
+unique(test_inventory_nwis_lakes$huc_number)

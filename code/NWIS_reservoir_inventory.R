17c5121fc86a9cb8d72c7983fbecfb8794629d05
Atshan <ratshan>
2021-05-07 13:25:40 -0500

    Function to pull NWIS reservoir site's temperature data inventory.

--- code/NWIS_reservoir_inventory.R
+++ code/NWIS_reservoir_inventory.R
@@ -1,44 +1,25 @@
 library(tidyverse)
 library(dataRetrieval)
 
-inventory_nwis_lakes <- function(nwis_pull_params, siteType, parameterCd){
-  #states_cd <- stateCd$STATE# Reterieving the U.S. state_codes.
-
+# Creating a function to retierve the NWIS inventory sites temperature data. 
+inventory_nwis_lakes <- function(site_type, parameter_cd){
+  
   hucs <- stringr::str_pad(1:21, width = 2, pad = "0")
-  nwis_pull_params$siteType <- siteType
-  nwis_pull_params$parameterCd <- parameterCd
-
-  # Inventory of reservoir sites from NWIS for lake's temperature data.
   lake_temp_inventory <- data.frame()
-
+  # Inventory of reservoir sites from NWIS for lake's temperature data.
   for(huc in hucs){
-
-    nwis_pull_params$huc = huc
-    state_lake_temp_inventory <- do.call(whatNWISdata, nwis_pull_params) %>%
-      select(c("agency_cd", "site_no", "station_nm", "dec_lat_va", "dec_long_va", "count_nu"))
-
+    state_lake_temp_inventory <- whatNWISdata(huc = huc,
+                                              siteType = site_type,
+                                              parameterCd = parameter_cd) %>%
+      select(c("agency_cd", "site_no", "station_nm", "dec_lat_va", 
+               "dec_long_va", "count_nu"))
+    
     lake_temp_inventory <- bind_rows(lake_temp_inventory, state_lake_temp_inventory)
-
+    
   }
-
+  return(lake_temp_inventory)
 }
-
-# Inventory of reservoir sites from NWIS for lake's temperature data.
-hucs <- stringr::str_pad(1:21, width = 2, pad = "0")
-lake_temp_inventory <- data.frame()
-
-for(huc in hucs){
-  state_lake_temp_inventory <- whatNWISdata(huc = huc,
-                                            #stateCd = state_cd,
-                                            siteType = "LK",
-                                            parameterCd="00010") %>%
-    select(c("agency_cd", "site_no", "station_nm", "dec_lat_va", "dec_long_va", "count_nu"))
-
-  lake_temp_inventory <- bind_rows(lake_temp_inventory, state_lake_temp_inventory)
-
-}
-summary(lake_temp_inventory)
 
 # Testing the inventory_nwis_lakes function
 test_inventory_nwis_lakes <- inventory_nwis_lakes(
-  siteType = "LK",parameterCd = "00010")
+  site_type = "LK" ,parameter_cd = "00010")
